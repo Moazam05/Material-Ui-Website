6963f73d376a6b1cf1c7ef8828439a9b0dedbc2c
Salman Moazam <salmanmoazam08@gmail.com>
2020-12-25 21:28:27 +0500

    AppBar & AppBar Transition

---
+++ src/components/ui/Header.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolar from '@material-ui/core/Toolbar';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+
+function ElevationScroll(props) {
+  const { children } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+
+export default function Header() {
+  return (
+    <>
+      <ElevationScroll>
+        <AppBar position='fixed'>
+          <Toolar>Arc Development</Toolar>
+        </AppBar>
+      </ElevationScroll>
+    </>
+  );
+}
