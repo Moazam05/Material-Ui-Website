03e296c17ead4d3df1e11cd3455c6b9eeff8d1f9
Salman Moazam <salmanmoazam08@gmail.com>
2020-12-26 00:34:16 +0500

    Styling The Tabs

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolar from '@material-ui/core/Toolbar';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import { makeStyles } from '@material-ui/core/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Button from '@material-ui/core/Button';
 
 import logo from '../../assets/logo.svg';
 
@@ -25,19 +28,64 @@ const useStyles = makeStyles((theme) => ({
     ...theme.mixins.toolbar,
     marginBottom: '',
   },
+
   logo: {
     height: '4em',
+  },
+
+  tabsContainer: {
+    marginLeft: 'auto',
+  },
+
+  tab: {
+    ...theme.typography.tab,
+    minWidth: 10,
+    marginLeft: '25px',
+  },
+
+  button: {
+    ...theme.typography.estimate,
+    borderRadius: '50px',
+    marginLeft: '50px',
+    marginRight: '25px',
+    height: '35px',
   },
 }));
 
 export default function Header() {
   const classes = useStyles();
+
+  const [value, setValue] = useState(0);
+
+  const handleChange = (e, value) => {
+    setValue(value);
+  };
+
   return (
     <>
       <ElevationScroll>
         <AppBar position='fixed'>
           <Toolar disableGutters>
             <img src={logo} alt='Company Logo' className={classes.logo} />
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              className={classes.tabsContainer}
+              //   indicatorColor='secondary'
+            >
+              <Tab className={classes.tab} label='Home' />
+              <Tab className={classes.tab} label='Services' />
+              <Tab className={classes.tab} label='The Revolution' />
+              <Tab className={classes.tab} label='About Us' />
+              <Tab className={classes.tab} label='Contact Us' />
+            </Tabs>
+            <Button
+              variant='contained'
+              color='secondary'
+              className={classes.button}
+            >
+              Free Estimate
+            </Button>
           </Toolar>
         </AppBar>
       </ElevationScroll>
